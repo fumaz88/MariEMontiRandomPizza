524845be610ac134db1ccbbcf5f5dc26e3abd391
Riccardo Fumagalli <Ric.fumagalli@hotmail.com>
2025-05-01 10:01:32 +0200

    added csv menù

--- MariEMontiRandomPizza/MainWindow.xaml.cs
+++ MariEMontiRandomPizza/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
@@ -27,114 +28,23 @@ namespace MariEMontiRandomPizza
 
         private void LoadPizzaMenu()
         {
-            // Aggiungi le pizze dal menu
-            // Primo gruppo - pizze standard
-            pizzaMenu.Add(new Pizza("ASPARAGI, UOVA E GRANA", 7.30m, "asparagi, uova, grana"));
-            pizzaMenu.Add(new Pizza("BIANCANEVE", 4.80m, "mozzarella senza pomodoro"));
-            pizzaMenu.Add(new Pizza("BEPPE", 5.30m, "ricotta, rucola, pomodoro a fette, olive, grana a scaglie"));
-            pizzaMenu.Add(new Pizza("BISMARK", 6.80m, "mozzarella, funghi, uovo, grana"));
-            pizzaMenu.Add(new Pizza("BOMBA", 7.30m, "tutto della pizza grana con fontina, prosciutto, funghi, parm. a fette, mozzarella"));
-            pizzaMenu.Add(new Pizza("BOSCAIOLA", 7.30m, "funghi, speck"));
-            pizzaMenu.Add(new Pizza("BRESAOLA, RUCOLA E GRANA", 7.30m, "bresaola, rucola, grana"));
-            pizzaMenu.Add(new Pizza("BUFALINA", 6.30m, "pomodoro, peschino, bufala, basilico"));
-            pizzaMenu.Add(new Pizza("CALZONE FARCITO", 6.80m, "ingredienti vari"));
-            pizzaMenu.Add(new Pizza("CALZONE MARGHERITA", 5.30m, "pomodoro, mozzarella"));
-            pizzaMenu.Add(new Pizza("CALZONE PROSCIUTTO", 6.00m, "pomodoro, mozzarella, prosciutto"));
-            pizzaMenu.Add(new Pizza("CALZONE PROSCIUTTO E FUNGHI", 6.30m, "pomodoro, mozzarella, prosciutto, funghi"));
-            pizzaMenu.Add(new Pizza("CALZONE RICOTTA", 6.30m, "pomodoro, mozzarella, ricotta"));
-            pizzaMenu.Add(new Pizza("CALZONE RICOTTA E SPINACI", 6.80m, "pomodoro, mozzarella, ricotta, spinaci"));
-            pizzaMenu.Add(new Pizza("CALZONE VERDURE", 7.80m, "pomodoro, mozzarella, verdure miste"));
-            pizzaMenu.Add(new Pizza("CAMPAGNOLA", 6.80m, "salsiccia e cipolla"));
-            pizzaMenu.Add(new Pizza("CAPRESE", 6.30m, "margherita con aggiunta di pomodoro e mozzarella a fette"));
-            pizzaMenu.Add(new Pizza("CAPRICCIOSA", 6.30m, "pomodoro, mozzarella, prosciutto, funghi, carciofi"));
-            pizzaMenu.Add(new Pizza("CARMELO", 7.80m, "metà calzone mozzarella e ricotta, metà pizza peperoni e cipolle"));
-            pizzaMenu.Add(new Pizza("CIAMBELLONE", 9.00m, "porcini, taleggio, crudo, scaglie di grana, rucola"));
-            pizzaMenu.Add(new Pizza("COZZE", 6.30m, "pomodoro, mozzarella, cozze"));
-            pizzaMenu.Add(new Pizza("CRUDO", 6.80m, "pomodoro, mozzarella, prosciutto crudo"));
-            pizzaMenu.Add(new Pizza("DIAVOLA", 6.80m, "salamino piccante, funghi, olive, capperi, peperoncino"));
-            pizzaMenu.Add(new Pizza("EMI", 6.80m, "ricotta, pomodoro a fette, rucola"));
-            pizzaMenu.Add(new Pizza("EMILIANO", 6.80m, "cipolla, pomodoro a fette, rucola"));
-            pizzaMenu.Add(new Pizza("FANTASIOSA", 8.30m, "taleggio, salsiccia, salami, piccante, pancetta"));
-            pizzaMenu.Add(new Pizza("FRUTTI DI MARE", 7.30m, "pomodoro, frutti di mare"));
-            pizzaMenu.Add(new Pizza("FUNGHI", 5.80m, "pomodoro, mozzarella, funghi"));
-            pizzaMenu.Add(new Pizza("FUNGHI E PANNA", 6.30m, "pomodoro, mozzarella, funghi, panna"));
-            pizzaMenu.Add(new Pizza("FURFANTE", 7.80m, "peperoni, salamino, olive, funghi, grana"));
-            pizzaMenu.Add(new Pizza("GAGLIARDA", 7.30m, "salame, capperi, olive, cipolla, acciughe"));
-            pizzaMenu.Add(new Pizza("GALANTE", 6.30m, "ricotta, spinaci"));
-            pizzaMenu.Add(new Pizza("GAMBERETTI", 7.30m, "pomodoro, mozzarella, gamberetti"));
+            try
+            {
+                // Percorso del file CSV (nella cartella dell'applicazione)
+                string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "menu-pizze.csv");
 
-            // Secondo gruppo - pizze con gamberetti e altri ingredienti
-            pizzaMenu.Add(new Pizza("GAMBERETTI E PANNA", 7.80m, "pomodoro, mozzarella, gamberetti, panna"));
-            pizzaMenu.Add(new Pizza("GAMBERETTI E RUCOLA", 7.80m, "pomodoro, mozzarella, gamberetti, rucola"));
-            pizzaMenu.Add(new Pizza("GAMBERETTI IN SALSA ROSA", 7.80m, "pomodoro, mozzarella, gamberetti, salsa rosa"));
-            pizzaMenu.Add(new Pizza("GHIOTTONA", 8.30m, "pomodoro, mozzarella, porcini, grana"));
-            pizzaMenu.Add(new Pizza("GUSTOSA", 7.80m, "pomodoro a fette, rucola, crudo, grana"));
-            pizzaMenu.Add(new Pizza("INNOMINATA", 7.30m, "crema di formaggio, pomodorini freschi, rucola, grana"));
-            pizzaMenu.Add(new Pizza("IMPIETRATRICE", 7.80m, "asparagi, uovo, porchetta, grana"));
-            pizzaMenu.Add(new Pizza("JULI", 6.80m, "capperi, olive"));
-            pizzaMenu.Add(new Pizza("MALIZIOSA", 7.30m, "pomodoro a fette, rucola"));
-            pizzaMenu.Add(new Pizza("MARE MONTI", 7.80m, "frutti di mare, porcini"));
-            pizzaMenu.Add(new Pizza("MARGHERITA", 4.80m, "pomodoro, mozzarella"));
-            pizzaMenu.Add(new Pizza("MARINELLA", 7.80m, "rucola, salmone, panna"));
-            pizzaMenu.Add(new Pizza("MARINARO", 5.30m, "aglio, olio, acciughe, capperi, olive, senza mozzarella"));
-            pizzaMenu.Add(new Pizza("MARINARA", 4.80m, "pomodoro, aglio, olio origano, senza mozzarella"));
-            pizzaMenu.Add(new Pizza("MELOMARE", 8.30m, "frutti di mare, tonno, gamberetti"));
-            pizzaMenu.Add(new Pizza("MUNTANERA", 6.30m, "asparagi e funghi"));
-            pizzaMenu.Add(new Pizza("NANI", 7.80m, "zucchine, bresaola, pomodoro a fette, grana"));
-            pizzaMenu.Add(new Pizza("NAPOLI", 5.80m, "acciughe"));
-            pizzaMenu.Add(new Pizza("NORDICA", 8.30m, "porchetta, panna, salmone"));
-            pizzaMenu.Add(new Pizza("ORTOLANA", 6.80m, "verdure alle piastra"));
-            pizzaMenu.Add(new Pizza("PANKETTA E UOVO", 7.30m, "pancetta, uovo, grana"));
-            pizzaMenu.Add(new Pizza("PAPARAZZI", 7.80m, "pomodorini, porcini, grana"));
-            pizzaMenu.Add(new Pizza("PARMIGIANA", 6.30m, "melanzane, grana"));
-            pizzaMenu.Add(new Pizza("PARTICOLARE", 8.30m, "porcini, salsiccia, salamino, pancetta, grana"));
-            pizzaMenu.Add(new Pizza("PATATINE", 5.80m, "pomodoro, mozzarella, patatine fritte"));
-            pizzaMenu.Add(new Pizza("PELTA", 8.30m, "ricotta, prosciutto, porcini, spinaci, grana"));
-            pizzaMenu.Add(new Pizza("PEPERONI", 5.80m, "pomodoro, mozzarella, peperoni"));
-            pizzaMenu.Add(new Pizza("PEPE", 8.30m, "ricotta, salsiccia, zola, porcini, grana"));
-            pizzaMenu.Add(new Pizza("PIPERISSA", 6.80m, "peperoni, speck, grana"));
-            pizzaMenu.Add(new Pizza("PESANTE", 8.30m, "salsiccia, zola, cipolle, salami, piccante"));
-            pizzaMenu.Add(new Pizza("PORCINI E GRANA", 7.30m, "pomodoro, mozzarella, porcini, grana"));
-            pizzaMenu.Add(new Pizza("PORCINI, BRESAOLA E GRANA", 6.80m, "pomodoro, mozzarella, porcini, bresaola, grana"));
-            pizzaMenu.Add(new Pizza("PROSCIUTTO", 5.60m, "pomodoro, mozzarella, prosciutto"));
-            pizzaMenu.Add(new Pizza("PROSCIUTTO E FUNGHI", 5.80m, "pomodoro, mozzarella, prosciutto, funghi"));
-            pizzaMenu.Add(new Pizza("PROSCIUTTO E PANNA", 5.80m, "pomodoro, mozzarella, prosciutto, panna"));
+                // Caricare le pizze dal file CSV
+                pizzaMenu = PizzaMenuLoader.LoadPizzasFromCsv(filePath);
 
-            // Terzo gruppo - pizze con formaggi e altri ingredienti
-            pizzaMenu.Add(new Pizza("PUGLIESE", 5.30m, "cipolla"));
-            pizzaMenu.Add(new Pizza("QUATTRO FORMAGGI", 6.30m, "pomodoro, mozzarella, formaggi misti"));
-            pizzaMenu.Add(new Pizza("QUATTRO STAGIONI", 6.30m, "pomodoro, mozzarella, prosciutto, funghi, carciofi, olive"));
-            pizzaMenu.Add(new Pizza("RICOTTA", 5.80m, "pomodoro, mozzarella, ricotta"));
-            pizzaMenu.Add(new Pizza("ROLLO", 6.80m, "calzone arrotolato ripieno di mozzarella, scamorza, speck, funghi, condito con robiola/ricotta, rucola, speck, grana"));
-            pizzaMenu.Add(new Pizza("ROMANA", 6.30m, "acciughe, capperi, olive"));
-            pizzaMenu.Add(new Pizza("RUCOLA E GRANA", 6.30m, "pomodoro, mozzarella, rucola, grana"));
-            pizzaMenu.Add(new Pizza("SALAME DOLCE", 6.30m, "pomodoro, mozzarella, salame dolce"));
-            pizzaMenu.Add(new Pizza("SALAMINO PICCANTE", 6.30m, "pomodoro, mozzarella, salamino piccante"));
-            pizzaMenu.Add(new Pizza("SALSICCIA", 6.30m, "pomodoro, mozzarella, salsiccia"));
-            pizzaMenu.Add(new Pizza("SALMONE", 6.730m, "pomodoro, mozzarella, salmone"));
-            pizzaMenu.Add(new Pizza("SALMONE E PANNA", 7.30m, "pomodoro, mozzarella, salmone, panna"));
-            pizzaMenu.Add(new Pizza("SCAMORZA", 6.30m, "pomodoro, mozzarella, scamorza"));
-            pizzaMenu.Add(new Pizza("SCAMORZA E SALAMINO PICCANTE", 6.80m, "pomodoro, mozzarella, scamorza, salamino piccante"));
-            pizzaMenu.Add(new Pizza("SCAMORZA E SPECK", 6.80m, "pomodoro, mozzarella, scamorza, speck"));
-            pizzaMenu.Add(new Pizza("SFIDIOSA", 6.80m, "ricotta, rucola, grana"));
-            pizzaMenu.Add(new Pizza("SICILIANA", 6.30m, "acciughe, capperi, olive, cipolle, senza mozzarella"));
-            pizzaMenu.Add(new Pizza("SILVIA", 6.730m, "brie, crudo"));
-            pizzaMenu.Add(new Pizza("SPECK", 6.30m, "pomodoro, mozzarella, speck"));
-            pizzaMenu.Add(new Pizza("SPINACI E GRANA", 6.30m, "pomodoro, mozzarella, spinaci, grana"));
-            pizzaMenu.Add(new Pizza("SPINACI, BRIE, SPECK", 6.730m, "pomodoro, mozzarella, spinaci, brie, speck"));
-            pizzaMenu.Add(new Pizza("SPINACI, PORCINI, TALEGGIO, GRANA", 8.30m, "pomodoro, mozzarella, spinaci, porcini, taleggio, grana"));
-            pizzaMenu.Add(new Pizza("SQUISITA", 6.80m, "mozzarella, pesche, rucola"));
-            pizzaMenu.Add(new Pizza("TIROLESE", 6.80m, "spianata, speck, grana"));
-            pizzaMenu.Add(new Pizza("TONNO", 6.30m, "pomodoro, mozzarella, tonno"));
-            pizzaMenu.Add(new Pizza("TONNO E CIPOLLE", 6.80m, "pomodoro, mozzarella, tonno, cipolle"));
-            pizzaMenu.Add(new Pizza("ULTIMA FOLLIA", 6.980m, "pasta della pizza ripiena con spaghetti allo scoglio"));
-            pizzaMenu.Add(new Pizza("VERDURE", 6.730m, "pomodoro, mozzarella, verdure miste"));
-            pizzaMenu.Add(new Pizza("VONGOLE", 6.30m, "pomodoro, mozzarella, vongole"));
-            pizzaMenu.Add(new Pizza("VULCANO", 8.30m, "capricciosa con doppia pasta"));
-            pizzaMenu.Add(new Pizza("WURSTER", 5.80m, "pomodoro, mozzarella, würstel"));
-            pizzaMenu.Add(new Pizza("WURSTER E PATATINE", 6.30m, "pomodoro, mozzarella, würstel, patatine"));
-            pizzaMenu.Add(new Pizza("ZOLA", 6.30m, "pomodoro, mozzarella, gorgonzola"));
-            pizzaMenu.Add(new Pizza("ZOLA E CRUDO", 7.30m, "pomodoro, mozzarella, gorgonzola, prosciutto crudo"));
+                // Aggiornare l'interfaccia utente con il menu caricato
+                //AggiornaInterfacciaMenu();
+
+                MessageBox.Show($"Menu caricato con successo! {pizzaMenu.Count} pizze trovate.", "Caricamento completato", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Errore durante il caricamento del menu: {ex.Message}", "Errore", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void InitializeGUI()
---
+++ MariEMontiRandomPizza/PizzaMenuLoader.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MariEMontiRandomPizza
+{
+    internal class PizzaMenuLoader
+    {
+        public static List<Pizza> LoadPizzasFromCsv(string filePath)
+        {
+            List<Pizza> pizzas = new List<Pizza>();
+
+            try
+            {
+                // Check if file exists
+                if (!File.Exists(filePath))
+                {
+                    throw new FileNotFoundException($"Il file menu non è stato trovato: {filePath}");
+                }
+
+                // Read all lines from the file
+                string[] lines = File.ReadAllLines(filePath);
+
+                // Skip header if present (first line with "Pizza,Prezzo")
+                int startIndex = lines[0].Trim().StartsWith("Pizza,Prezzo", StringComparison.OrdinalIgnoreCase) ? 1 : 0;
+
+                // Process each line
+                for (int i = startIndex; i < lines.Length; i++)
+                {
+                    string line = lines[i].Trim();
+                    if (string.IsNullOrEmpty(line)) continue;
+
+                    // Parse the line
+                    Pizza pizza = ParseCsvLine(line);
+                    if (pizza != null)
+                    {
+                        pizzas.Add(pizza);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Errore durante la lettura del file CSV: {ex.Message}");
+                throw;
+            }
+
+            return pizzas;
+        }
+
+        // Parse a single CSV line to create a Pizza object
+        private static Pizza ParseCsvLine(string line)
+        {
+            try
+            {
+                // Handle special case for quoted entries
+                List<string> values = new List<string>();
+                bool inQuotes = false;
+                int startPos = 0;
+
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (line[i] == '"')
+                    {
+                        inQuotes = !inQuotes;
+                    }
+                    else if (line[i] == ',' && !inQuotes)
+                    {
+                        // Found a comma outside of quotes, this is a delimiter
+                        values.Add(line.Substring(startPos, i - startPos).Trim());
+                        startPos = i + 1;
+                    }
+                }
+
+                // Add the last segment
+                values.Add(line.Substring(startPos).Trim());
+
+                // If we found exactly 2 segments (name and price)
+                if (values.Count == 2)
+                {
+                    string fullPizzaName = values[0].Trim('"');
+                    string priceStr = values[1].Trim();
+
+                    // Convert price string to decimal (handle different formats)
+                    decimal price = ParsePrice(priceStr);
+
+                    // Extract ingredients from the name if possible
+                    string name, ingredients;
+                    ExtractNameAndIngredients(fullPizzaName, out name, out ingredients);
+
+                    return new Pizza(name, price, ingredients);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Errore durante l'analisi della riga CSV: {line}. Errore: {ex.Message}");
+            }
+
+            return null;
+        }
+
+        // Helper method to extract both clean name and ingredients from pizza full name
+        private static void ExtractNameAndIngredients(string fullPizzaName, out string name, out string ingredients)
+        {
+            // Check if there are parentheses in the name
+            int openParenIndex = fullPizzaName.IndexOf('(');
+
+            if (openParenIndex > 0)
+            {
+                // Extract the clean name (part before parenthesis)
+                name = fullPizzaName.Substring(0, openParenIndex).Trim();
+
+                // Extract ingredients (content inside parentheses)
+                int closeParenIndex = fullPizzaName.LastIndexOf(')');
+                if (closeParenIndex > openParenIndex)
+                {
+                    ingredients = fullPizzaName.Substring(openParenIndex + 1, closeParenIndex - openParenIndex - 1).Trim();
+                }
+                else
+                {
+                    // No closing parenthesis found
+                    ingredients = string.Empty;
+                }
+            }
+            else
+            {
+                // No parentheses, use the full name as the name and empty ingredients
+                name = fullPizzaName;
+                ingredients = string.Empty;
+            }
+        }
+
+        // Helper method to parse price strings like "€ 7.80" or "7,80€"
+        private static decimal ParsePrice(string priceStr)
+        {
+            // Remove currency symbol and trim
+            priceStr = priceStr.Replace("€", "").Trim();
+
+            // Replace comma with dot for decimal point if needed
+            priceStr = priceStr.Replace(',', '.');
+
+            // Parse to decimal
+            if (decimal.TryParse(priceStr, NumberStyles.Any, CultureInfo.InvariantCulture, out decimal price))
+            {
+                return price;
+            }
+
+            throw new FormatException($"Impossibile convertire '{priceStr}' in un prezzo valido.");
+        }
+    }
+}
